363d9246b800dbd3675ba870bb3389d50ca92997
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-06-29 17:30:21 -0400

    still trying to work out win/loss counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,44 @@
 $(document).ready(() => {
-    // const defaultGameState = () => {
-        //generate main game number between 19 - 120
-        let mainNum = (min, max) => {
-            return Math.floor(Math.random() * (120 - 19 + 1) + 19);
-        }
+    //generate main game number between 19 - 120
+    let mainNum = (min, max) => {
+        return Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    }
+    $('.randNum').text(mainNum);
+    console.log(mainNum);
 
-        $('.randNum').text(mainNum);
+    //sets user's base number to zero
+    let baseNum = 0;
+    $('.userNum').attr('data-value', baseNum);
+    $('.userNum').text(baseNum);
 
-        //sets user's base number to zero
-        let baseNum = 0;
-        $('.userNum').text(baseNum);
+    //sets user's base win count to zero
+    let wins = 0;
+    $('.winCount').text(wins);
 
-        //sets user's base win count to zero
-        let baseWinNum = 0;
-        $('.winCount').text(baseWinNum);
+    //sets user's base loss count to zero
+    let losses = 0;
+    $('.lossCount').text(losses);
 
-        //sets user's base loss count to zero
-        let baseLossNum = 0;
-        $('.lossCount').text(baseLossNum);
+    //generates random numbers for the crystals
+    const crysNumOne = () => {
+        return Math.floor((Math.random() * 12) + 1);
+    };
+    $('.crystalOne').attr('data-value', crysNumOne());
 
-        //generates random numbers for the crystals
-        const crysNumOne = () => {
-            return Math.floor((Math.random() * 12) + 1);
-        };
-        $('.crystalOne').attr('data-value', crysNumOne());
+    const crysNumTwo = () => {
+        return Math.floor((Math.random() * 12) + 1);
+    };
+    $('.crystalTwo').attr('data-value', crysNumTwo());
 
-        const crysNumTwo = () => {
-            return Math.floor((Math.random() * 12) + 1);
-        };
-        $('.crystalTwo').attr('data-value', crysNumTwo());
+    const crysNumThree = () => {
+        return Math.floor((Math.random() * 12) + 1);
+    };
+    $('.crystalThree').attr('data-value', crysNumThree());
 
-        const crysNumThree = () => {
-            return Math.floor((Math.random() * 12) + 1);
-        };
-        $('.crystalThree').attr('data-value', crysNumThree());
-
-        const crysNumFour = () => {
-            return Math.floor((Math.random() * 12) + 1);
-        };
-        $('.crystalFour').attr('data-value', crysNumFour());
-
-    // }
-
-    // defaultGameState();
+    const crysNumFour = () => {
+        return Math.floor((Math.random() * 12) + 1);
+    };
+    $('.crystalFour').attr('data-value', crysNumFour());
 
     //-----------------------------------------------------------------------------------------
     //-----------------------------------------------------------------------------------------
@@ -53,7 +49,6 @@ $(document).ready(() => {
     $('.crystalOne').on('click', function () {
         let crystalValueOne = parseInt($('.crystalOne').attr('data-value'));
         $('.userNum').text(baseNum += crystalValueOne);
-
     });
 
     //adds crystal two's randum number to the user number
@@ -77,27 +72,13 @@ $(document).ready(() => {
     //-----------------------------------------------------------------------------------------
     //-----------------------------------------------------------------------------------------
 
-    // win/loss counter code 
-    // TODO outline, needs refactor
-    // if ()
-    // let winCount = () => {
-    //     baseWinNum++;
-    // }
+    // win/loss counter
+    if (baseNum === mainNum) {
+        wins++;
+    };
 
-    // let lossCount = () => {
-    //     baseLossNum++;
-    // }
-
-    // let winLossF = () => {
-    //     if (baseNum === mainNum) {
-    //         winCount();
-    //     }
-    //     else {
-    //         lossCount();
-    //     }
-    // }
-
-    // console.log(winLossF());
-
+    if (baseNum > mainNum) {
+        losses++;
+    };
 
 });
